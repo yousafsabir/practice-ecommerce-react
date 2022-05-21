6473616e1a870e547a806ab503dab73e2f3b1e32
yousafsabir <yousafsabir430516@gmail.com>
2022-05-22 00:43:21 +0500

    navbar style2

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -41,183 +41,175 @@ const Navbar = (props) => {
                     <div className="bottom">
                         <img src={Logo} alt="" className="logo" />
 
-                        <nav>
-                            <ul className="fs-200 bottom-nav">
-                                <li className="nav-padding">
-                                    <a
-                                        href="/"
-                                        className={
-                                            activeNav === "home" ? "bold" : ""
-                                        }
-                                        onClick={() => {
-                                            setActiveNav("/");
-                                        }}
-                                    >
-                                        Home
-                                    </a>
-                                </li>
-                                <li className="nav-padding dropdown">
-                                    <div
-                                        className="flex"
-                                        style={{ gap: "0.25rem" }}
-                                    >
-                                        <p>Services</p>
-                                        <VscTriangleDown />
-                                    </div>
-                                    <ul className="sub-menu">
-                                        <li>
-                                            <a href="/">Service 1</a>
-                                        </li>
-                                        <li>
-                                            <a href="/">Service 2</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li className="nav-padding">
-                                    <a
-                                        href="/"
-                                        className={
-                                            activeNav === "blog" ? "bold" : ""
-                                        }
-                                        onClick={() => {
-                                            setActiveNav("blog");
-                                        }}
-                                    >
-                                        About
-                                    </a>
-                                </li>
-                                <li className="nav-padding">
-                                    <a
-                                        href="contactos"
-                                        className={
-                                            activeNav === "contactos"
-                                                ? "bold"
-                                                : ""
-                                        }
-                                        onClick={() => {
-                                            setActiveNav("contactos");
-                                        }}
-                                    >
-                                        Contact Us
-                                    </a>
-                                </li>
-                                <li
-                                    className={
-                                        activeNav === "home"
-                                            ? "bottom-line"
-                                            : ""
-                                    }
-                                ></li>
-                                <li></li>
-                                <li
-                                    className={
-                                        activeNav === "blog"
-                                            ? "bottom-line"
-                                            : ""
-                                    }
-                                ></li>
-                                <li
-                                    className={
-                                        activeNav === "contactos"
-                                            ? "bottom-line"
-                                            : ""
-                                    }
-                                ></li>
-                            </ul>
-                        </nav>
-
-                        <div
-                            className={`buttons ${
-                                profile ? "buttonsHide" : ""
-                            }`}
-                        >
-                            <a
-                                href="/"
-                                className="btn-outline transition btn-shadow"
-                            >
-                                Sign Up
-                            </a>
-                            <a
-                                href="/"
-                                className="btn-contain transition btn-shadow"
-                            >
-                                Login
-                            </a>
-                        </div>
-                        <div
-                            className={`profile ${
-                                profile ? "" : "profileHide"
-                            }`}
-                            style={{
-                                alignItems: "center",
-                                gap: "22px",
-                            }}
-                        >
-                            <div
-                                className="profile"
-                                style={{
-                                    display: "flex",
-                                    alignItems: "center",
-                                    gap: "10px",
-                                }}
-                            >
-                                <img src={ProfilePic} alt="" />
-                                <ul style={{ margin: "0", padding: "0" }}>
-                                    <li className="profile-dropdown">
-                                        <div
-                                            style={{
-                                                display: "flex",
-                                                alignItems: "center",
-                                                padding: "6px 0",
-                                                gap: "4px",
-                                                cursor: "pointer",
+                        <div className="bottom-right">
+                            <nav>
+                                <ul className="fs-200 bottom-nav">
+                                    <li className="nav-padding">
+                                        <a
+                                            href="/"
+                                            className={
+                                                activeNav === "home"
+                                                    ? "bold"
+                                                    : ""
+                                            }
+                                            onClick={() => {
+                                                setActiveNav("/");
                                             }}
                                         >
-                                            <p
-                                                className="fs-100 bold clr-black-light"
-                                                style={{ marginBottom: "0" }}
-                                            >
-                                                Jessica
-                                            </p>
-                                            <VscTriangleDown
-                                                style={{ width: "8px" }}
-                                            />
-                                        </div>
-                                        <ul className="profile-submenu">
-                                            <li className="upper-items bold fs-200 clr-black-main">
-                                                Uni-Desk
-                                            </li>
-                                            <li className="upper-items bold fs-200 clr-black-main">
-                                                Minha Estadia
-                                            </li>
-                                            <li className="upper-items bold fs-200 clr-black-main">
-                                                Favoritos
-                                            </li>
-                                            <li className="upper-items bold fs-200 clr-black-main">
-                                                Caixa de Entrada (5)
-                                            </li>
-                                            <li className="upper-items bold fs-200 clr-black-main">
-                                                Notificações (3)
-                                            </li>
-                                            <li
-                                                style={{
-                                                    width: "100%",
-                                                    height: "1px",
-                                                    backgroundColor: "#505046",
-                                                }}
-                                            ></li>
-                                            <li className="fs-100 clr-black-extralight">
-                                                Conta
-                                            </li>
-                                            <li className="fs-100 clr-black-extralight">
-                                                Ajuda
-                                            </li>
-                                            <li className="fs-100 clr-black-extralight">
-                                                Sair
-                                            </li>
-                                        </ul>
+                                            Home
+                                        </a>
                                     </li>
+                                    <li className="nav-padding">
+                                        <a
+                                            href="/"
+                                            className={
+                                                activeNav === "blog"
+                                                    ? "bold"
+                                                    : ""
+                                            }
+                                            onClick={() => {
+                                                setActiveNav("blog");
+                                            }}
+                                        >
+                                            About
+                                        </a>
+                                    </li>
+                                    <li className="nav-padding">
+                                        <a
+                                            href="contactos"
+                                            className={
+                                                activeNav === "contactos"
+                                                    ? "bold"
+                                                    : ""
+                                            }
+                                            onClick={() => {
+                                                setActiveNav("contactos");
+                                            }}
+                                        >
+                                            Contact Us
+                                        </a>
+                                    </li>
+                                    <li
+                                        className={
+                                            activeNav === "home"
+                                                ? "bottom-line"
+                                                : ""
+                                        }
+                                    ></li>
+                                    <li></li>
+                                    <li
+                                        className={
+                                            activeNav === "blog"
+                                                ? "bottom-line"
+                                                : ""
+                                        }
+                                    ></li>
+                                    <li
+                                        className={
+                                            activeNav === "contactos"
+                                                ? "bottom-line"
+                                                : ""
+                                        }
+                                    ></li>
                                 </ul>
+                            </nav>
+
+                            <div
+                                className={`buttons ${
+                                    profile ? "buttonsHide" : ""
+                                }`}
+                            >
+                                <a
+                                    href="/"
+                                    className="btn-outline transition btn-shadow"
+                                >
+                                    Sign Up
+                                </a>
+                                <a
+                                    href="/"
+                                    className="btn-contain transition btn-shadow"
+                                >
+                                    Login
+                                </a>
+                            </div>
+                            <div
+                                className={`profile ${
+                                    profile ? "" : "profileHide"
+                                }`}
+                                style={{
+                                    alignItems: "center",
+                                    gap: "22px",
+                                }}
+                            >
+                                <div
+                                    className="profile"
+                                    style={{
+                                        display: "flex",
+                                        alignItems: "center",
+                                        gap: "10px",
+                                    }}
+                                >
+                                    <img src={ProfilePic} alt="" />
+                                    <ul style={{ margin: "0", padding: "0" }}>
+                                        <li className="profile-dropdown">
+                                            <div
+                                                style={{
+                                                    display: "flex",
+                                                    alignItems: "center",
+                                                    padding: "6px 0",
+                                                    gap: "4px",
+                                                    cursor: "pointer",
+                                                }}
+                                            >
+                                                <p
+                                                    className="fs-100 bold clr-black-light"
+                                                    style={{
+                                                        marginBottom: "0",
+                                                    }}
+                                                >
+                                                    Jessica
+                                                </p>
+                                                <VscTriangleDown
+                                                    style={{ width: "8px" }}
+                                                />
+                                            </div>
+                                            <ul className="profile-submenu">
+                                                <li className="upper-items bold fs-200 clr-black-main">
+                                                    Uni-Desk
+                                                </li>
+                                                <li className="upper-items bold fs-200 clr-black-main">
+                                                    Minha Estadia
+                                                </li>
+                                                <li className="upper-items bold fs-200 clr-black-main">
+                                                    Favoritos
+                                                </li>
+                                                <li className="upper-items bold fs-200 clr-black-main">
+                                                    Caixa de Entrada (5)
+                                                </li>
+                                                <li className="upper-items bold fs-200 clr-black-main">
+                                                    Notificações (3)
+                                                </li>
+                                                <li
+                                                    style={{
+                                                        width: "100%",
+                                                        height: "1px",
+                                                        backgroundColor:
+                                                            "#505046",
+                                                    }}
+                                                ></li>
+                                                <li className="fs-100 clr-black-extralight">
+                                                    Conta
+                                                </li>
+                                                <li className="fs-100 clr-black-extralight">
+                                                    Ajuda
+                                                </li>
+                                                <li className="fs-100 clr-black-extralight">
+                                                    Sair
+                                                </li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
